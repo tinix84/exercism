e95b9b843f83b5d5c48aafb7f6b02f0e039ad938
Riccardo Tinivella <tinix84@gmail.com>
2019-09-09 22:38:03 +0200

    ex3 py solved

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,15 @@
 def convert(number):
-    pass
+    
+    string_to_return = ''
+
+    if (number % 3) == 0:
+        string_to_return += "Pling"
+    if (number % 5) == 0:
+        string_to_return += "Plang"
+    if (number % 7) == 0:
+        string_to_return += "Plong"
+    if string_to_return == '':
+        string_to_return = str(number)
+
+    return str(string_to_return)
+        

ceb85112901a4c46214934e1ff9d5f843b1fde35
Riccardo Tinivella <tinix84@gmail.com>
2019-09-09 23:18:40 +0200

    add other solution from forum

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -11,3 +11,9 @@ def two_fer(name=None):
     else:
         string_to_say = ("One for %s, one for me." % name)
     return string_to_say
+
+def two_fer_dRpSY(name="you"):
+    #return "One for " + name + ", one for me."
+    #return "One for %s, one for me." % name
+    #return "One for {}, one for me.".format(name)
+    return f"One for {name}, one for me."
